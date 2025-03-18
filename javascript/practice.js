3cad86c973c981338a7323f5686fb3f50f2dff35
shiri357 <ssiri6886@gmail.com>
2025-03-18 10:15:04 +0530

    js

--- javascript/practice.js
+++ javascript/practice.js
@@ -23,7 +23,7 @@ function findMax(arr) {
 }
 
 console.log(findMax([3, 1, 4, 1, 5, 9]));  // Output: 9
-// 
+//4 Write a function to merge two sorted arrays.
 function removeDuplicates(arr) {
     let newarr = [];
     for (let i of arr) {
@@ -54,7 +54,111 @@ function findMissingNumber(arr) {
 }
 
 console.log(findMissingNumber([1, 2, 4, 5]));  // Output: 3
-// 
+// 10 question
+function countOccurrences(str) {
+    const charCount = {};
+    for (let char of str) {
+        charCount[char] = (charCount[char] || 0) + 1;
+    }
+    return charCount;
+}
+
+console.log(countOccurrences("hello"));
+// Output: { h: 1, e: 1, l: 2, o: 1 }
+
+//14. Longest Word in a String
+function longestWord(str) {
+    return str.split(' ').reduce((a, b) => a.length > b.length ? a : b);
+}
+
+console.log(longestWord("The quick brown fox jumps over the lazy dog"));
+// Output: "quick"
+
+
+//15. Array Intersection
+
+
+function arrayIntersection(arr1, arr2) {
+    return arr1.filter(value => arr2.includes(value));
+}
+
+console.log(arrayIntersection([1, 2, 3], [2, 3, 4]));
+// Output: [2, 3]
+// 17 Write a function to merge two sorted arrays.
+function mergeSortedArrays(arr1, arr2) {
+    return [...arr1, ...arr2].sort((a, b) => a - b);
+}
+
+console.log(mergeSortedArrays([1, 3, 5], [2, 4, 6]));
+// Output: [1, 2, 3, 4, 5, 6]
+
+
+
+
+
+//1 Find the output of the following code:
+ console.log(1 + "2" + "2");  //122
+console.log(1 + +"2" + "2"); //32
+console.log(1 + -"1" + "2"); //02
+console.log(+"1" + "1" + "2");//312
+console.log("A" - "B" + "2");//Nan2
+console.log("A" - "B" + 2);//Nan2
+
+//2 What will be the output of the following?
+console.log(0.1 + 0.2 === 0.3);//false
+// What will be the output?
+let a = {};
+let b = { key: "b" };
+let c = { key: "c" };
+
+a[b] = 123;
+a[c] = 456;
+
+console.log(a[b]);//456
+// 3What does this function return?
+
+function foo() {
+    return
+    {
+        name: "John"
+    };
+}
+console.log(foo());// name: "John"
+//4 How do you make this work?
+function sum(a) {
+    return function(b) {
+        return function(c) {
+            return a + b + c;
+        }
+    }
+}
+
+console.log(sum(2)(3)(4)); // Expected output: 9
+8. /*What will be the output?*/
+    
+    
+    console.log(typeof NaN); //number
+    
+    
+    
+9. /*How to check if an object is empty in JavaScript?*/
+    
+    
+function isEmpty(obj) {
+    return Object.keys(obj).length === 0;
+}
+
+/*10 What will be the output?*/
+function test() {
+    console.log(a);
+    var a = 5;
+    console.log(a);
+}
+test();//undefined
+    
+    
+    
+
 
 
 
